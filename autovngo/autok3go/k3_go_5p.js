e98f7a4cd7ef57398cfd79259c0c3204da8e17c7
danghung <danghung0201@gmail.com>
2023-12-14 10:15:08 +0700

    add

--- autovngo/autok3go/k3_go_5p.js
+++ autovngo/autok3go/k3_go_5p.js
@@ -4,7 +4,7 @@ let db = require('knex')({
         host: '127.0.0.1',
         port: 3306,
         user: 'root',
-        password: '',
+        password: 'PokerVn@123P',
         database: 'bot_telegram'
     }
 })
